7fdc5ace97ba66d0e3249f5114b9230fe9ee77df
v.adhithyan <aavispeaks@gmail.com>
2016-03-20 17:22:29 +0530

    initial working version tested (previous button not working)

---
+++ Cricket Score Applet/Score.swift
@@ -0,0 +1,16 @@
+//
+//  Score.swift
+//  Cricket Score Applet
+//
+//  Created by Adhithyan Vijayakumar on 20/03/16.
+//  Copyright © 2016 Adhithyan Vijayakumar. All rights reserved.
+//
+
+import Foundation
+
+struct Score{
+    let title: String
+    let description: String
+    let link: String
+    //static let all: [Score]
+}
--- Cricket Score Applet/ScoreViewController.swift
+++ Cricket Score Applet/ScoreViewController.swift
@@ -13,21 +13,19 @@ class ScoreViewController: NSViewController, NSXMLParserDelegate {
     
     var parser = NSXMLParser()
     var posts = NSMutableArray()
-    var elements = NSMutableDictionary()
-    var element = NSString()
-    var contentTitle = NSMutableString()
     var content = NSMutableString()
-    var link = NSMutableString()
+    var currentItem = ""
+    var itemCount = 0
     var currentScoreIndex: Int = 0{
         didSet{
             updateScore()
         }
     }
+    //let scores = Score.all
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.beginParsing()
-        print(posts.count)
     }
     
     
@@ -38,69 +36,61 @@ class ScoreViewController: NSViewController, NSXMLParserDelegate {
         parser.parse()
     }
     
-    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]){
-        element = elementName
-        
-        if (elementName as NSString).isEqualToString("item"){
-            elements = NSMutableDictionary()
-            elements = [:]
-            
-            contentTitle = NSMutableString()
-            contentTitle = ""
-            
-            link = NSMutableString()
-            link = ""
-            
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+    
+        if(elementName == "item"){
+            itemCount++;
             content = NSMutableString()
-            content = ""
+        }
+        
+        switch(elementName){
+            case "title":
+                currentItem = "title"
+                break
+            case "description":
+                currentItem = "description"
+                break
+            case "link":
+                currentItem = "link"
+                break
+            default:
+                break
         }
     }
-
     
     func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-       
         
-        if(elementName as NSString).isEqualToString("item"){
-            if (contentTitle.length == 0){
-                elements.setObject(contentTitle, forKey: "title")
-            }
-            
-            if(content.length == 0){
-                elements.setObject(content, forKey: "description")
-            }
-            
-            if(link.length == 0){
-                elements.setObject(link, forKey: "link")
-            }
-            posts.addObject(elements)
+        if(elementName == "item"){
+            itemCount--;
+            currentItem = ""
+            posts.addObject(content)
         }
     }
     
-    func parser(parser: NSXMLParser, foundCharacters string: String)
-    {
-        if element.isEqualToString("title") {
-            contentTitle.appendString(string)
-        } else if element.isEqualToString("description") {
-            content.appendString(string)
-        } else if element.isEqualToString("link"){
-            link.appendString(string)
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        
+        if(itemCount == 1){
+            if(currentItem == "title" || currentItem == "description" || currentItem == "link"){
+                content.appendString(string + "\n")
+                print(string)
+            }
         }
     }
     
     func updateScore(){
-        scoreCard.stringValue = posts[currentScoreIndex] as! String
+        scoreCard.stringValue = String(posts[currentScoreIndex])
     }
+    
 }
 
 extension ScoreViewController{
     @IBAction func goLeft(sender: NSButton){
         currentScoreIndex = (currentScoreIndex - 1 + posts.count) % (posts.count)
-        print(currentScoreIndex)
+        print(posts[currentScoreIndex])
     }
     
     @IBAction func goRight(sender: NSButton){
         currentScoreIndex = (currentScoreIndex + 1) % (posts.count)
-        print(currentScoreIndex)
     }
 }
 
